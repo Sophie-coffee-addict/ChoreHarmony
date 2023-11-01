d3d5ccffbb5a603ffafadf3b9c06ce76e8c5825d
sophiecao <sophiecao@localhost>
2023-11-01 15:52:26 +1300

    Added empty JavaDoc comment template for Main class.

--- ChoreHarmony/src/application/Main.java
+++ ChoreHarmony/src/application/Main.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
+/**
+ * 
+ */
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
