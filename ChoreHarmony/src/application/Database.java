7b7489d0a8c73c76cd36c3834ef849ffc416601a
sophiecao <sophiecao@localhost>
2023-11-02 10:53:38 +1300

    Added the chores and users arraylist to Database.java

--- ChoreHarmony/src/application/Database.java
+++ ChoreHarmony/src/application/Database.java
@@ -1,6 +1,11 @@
 package application;
 
+import java.util.ArrayList;
+
 public class Database {
+	
+	private ArrayList<String> chores = new ArrayList<>();
+	private ArrayList<String> users = new ArrayList<>();
 	
 	
 	public void loadData() {
