1ab9a612429d3922af540510dd14b314ecadafaf
richardbaldwin <richardbaldwin@localhost>
2023-11-02 12:25:01 +1300

    More cleanup

--- ChoreHarmony/src/application/LoginPage.java
+++ ChoreHarmony/src/application/LoginPage.java
@@ -8,7 +8,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
+
 
 public class LoginPage extends Pane {
 	
